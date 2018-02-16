f45422531dd747e35165efc935a122210b905410
DontPanic494 <dontpanic494@gmail.com>
2018-02-15 21:17:46 -0500

    constructor and state

--- src/app.jsx
+++ src/app.jsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './App.css';
 
 class App extends Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    deadline: 'December 25, 2018'
+  }
+}
+
   render() {
     return (
       <div className="App">
-        <div className="App-title">Countdown to December 25, 2018</div>
+        <div className="App-title">Countdown to {this.state.deadline}</div>
         <div>
           <div className="Clock-days">Days</div>
           <div className="Clock-hours">Hours</div>
