3b9d087845d1479db13efe91e444b2d3a3217ed3
DontPanic494 <dontpanic494@gmail.com>
2018-02-15 21:06:36 -0500

    added css and patched filenames

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>Countdown Champ, App Component</div>
+      <div className="App">
+        <div>Countdown to December 25, 2018</div>
+        <div>
+          <div>Days</div>
+          <div>Hours</div>
+          <div>Minutes</div>
+          <div>Seconds</div>
+        </div>
+        <div>
+          <input placeholder='new date'/>
+          <button>Submit</button>
+        </div>
+      </div>
     )
   }
 }
